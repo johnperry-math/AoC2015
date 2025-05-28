f0ac81e3ccdc02c38deac12d3b290e8c97d2deaa
John Perry <devotus@yahoo.com>
2025-05-28 01:46:32 -0400

    some cleanup on day 07

--- day07/src/day07.adb
+++ day07/src/day07.adb
@@ -31,25 +31,27 @@ procedure Day07 is
       end case;
    end record;
 
+   type Gate_Enum is
+     (Value, And_Gate, Left_Shift, Not_Gate, Or_Gate, Right_Shift);
+
    type Binary_Gate_Record is record
       Left, Right : Input_Record;
    end record;
 
-   type Gate_Enum is
-     (Value, And_Gate, Left_Shift, Not_Gate, Or_Gate, Right_Shift);
-
    type Feed_Record (Gate : Gate_Enum := Value) is record
       case Gate is
          when Value | Not_Gate =>
-            Value : Input_Record;
+            Input : Input_Record;
 
          when And_Gate | Or_Gate | Left_Shift | Right_Shift =>
-            Values : Binary_Gate_Record;
+            Inputs : Binary_Gate_Record;
 
       end case;
    end record;
 
    subtype Name_Range is Character range '`' .. 'z';
+   --  this hack makes it easy to use a _relatively_ small array
+   --  as a mapping of names to gates or values
 
    type Wire_Array is array (Name_Range, Name_Range) of Feed_Record;
 
@@ -61,6 +63,12 @@ procedure Day07 is
    function Read_Operand
      (Line : String; Start, Stop : Positive) return Input_Record
    is
+      --  reads the operand in the indicated position of Line
+      --  and returns an appropriate input record
+      --
+      --  the convention for a one-character name is '`x'
+      --  rather than 'x`', and spaces are unfortunately
+      --  not possible with this approach
       Name          : Name_String;
       Operand_Value : Value_Type;
    begin
@@ -79,6 +87,9 @@ procedure Day07 is
    end Read_Operand;
 
    function Is_Direct_Feed (Line : String) return Boolean is
+      --  return True if and only if this has the form
+      --      first -> second
+      --  i.e., no operands
       Position : Positive := Line'First;
    begin
       while Line (Position) /= ' ' loop
@@ -88,6 +99,8 @@ procedure Day07 is
    end Is_Direct_Feed;
 
    function Read_Direct_Feed (Line : String) return Feed_Record is
+      --  reads a direct feed (see Is_Direct_Feed) and returns
+      --  a corresponding record
       Position : Positive := Line'First;
    begin
       while Line (Position) /= ' ' loop
@@ -96,13 +109,13 @@ procedure Day07 is
       return
         Feed_Record'
           (Gate  => Value,
-           Value => Read_Operand (Line, Line'First, Position - 1));
+           Input => Read_Operand (Line, Line'First, Position - 1));
    end Read_Direct_Feed;
 
    function Read_Not_Feed (Line : String) return Feed_Record
    is (Feed_Record'
          (Gate  => Not_Gate,
-          Value =>
+          Input =>
             Input_Record'
               (Kind => Gate,
                Feed =>
@@ -114,35 +127,46 @@ procedure Day07 is
    --  this proved very useful when debugging
 
    function Read_Binary_Feed (Line : String) return Feed_Record is
+      --  this will fail if Line does not hold a binary operator
+      --  (AND, OR, LSHIFT, RSHIFT)
       Start_Read, Stop_Read : Positive := Line'First;
       First_Op, Second_Op   : Input_Record;
       Gate                  : Gate_Enum;
    begin
+      --  find and read the first operand
       while Line (Stop_Read) /= ' ' loop
          Stop_Read := @ + 1;
       end loop;
       First_Op := Read_Operand (Line, Start_Read, Stop_Read - 1);
+
+      --  determine the operator
       case Line (Stop_Read + 1) is
          when 'A' =>
+            --  AND
             Gate := And_Gate;
             Start_Read := Stop_Read + 5;
 
          when 'O' =>
+            --  OR
             Gate := Or_Gate;
             Start_Read := Stop_Read + 4;
 
          when 'L' =>
+            --  LSHIFT
             Gate := Left_Shift;
             Start_Read := Stop_Read + 8;
 
          when 'R' =>
+            --  RSHIFT
             Gate := Right_Shift;
             Start_Read := Stop_Read + 8;
 
          when others =>
+            --  not binary; how are we here?!?
             raise Invalid_Gate with Line;
       end case;
 
+      --  find and read the second operator
       Stop_Read := Start_Read;
       while Line (Stop_Read) /= ' ' loop
          Stop_Read := @ + 1;
@@ -154,22 +178,22 @@ procedure Day07 is
            when And_Gate =>
              Feed_Record'
                (And_Gate,
-                Values =>
+                Inputs =>
                   Binary_Gate_Record'(Left => First_Op, Right => Second_Op)),
            when Or_Gate =>
              Feed_Record'
                (Or_Gate,
-                Values =>
+                Inputs =>
                   Binary_Gate_Record'(Left => First_Op, Right => Second_Op)),
            when Left_Shift =>
              Feed_Record'
                (Left_Shift,
-                Values =>
+                Inputs =>
                   Binary_Gate_Record'(Left => First_Op, Right => Second_Op)),
            when Right_Shift =>
              Feed_Record'
                (Right_Shift,
-                Values =>
+                Inputs =>
                   Binary_Gate_Record'(Left => First_Op, Right => Second_Op)),
            when others => raise Invalid_Gate with Line);
    end Read_Binary_Feed;
@@ -201,6 +225,9 @@ procedure Day07 is
    --  SECTION
    --  parts 1 and 2
 
+   --  SUBSECTION
+   --  memoizing wire values
+
    type Maybe_Value (Holds_Value : Boolean := False) is record
       case Holds_Value is
          when True =>
@@ -213,40 +240,49 @@ procedure Day07 is
 
    Wire_Values : array (Name_Range, Name_Range) of Maybe_Value;
 
+   --  SUBSECTION
+   --  recursive evaluation
+
    function Evaluate (Input : Input_Record) return Value_Type is
    begin
+      --  if the value is immediate, return it
       if Input.Kind = Value then
          return Input.Value;
       end if;
+
+      --  not immediate but named; extract the name and evalute recursively
       declare
          Name       : constant Name_String := Input.Feed;
          Wire renames Wires (Name (1), Name (2));
          Name_Value : Value_Type;
       begin
+         --  if we already know the value, return it
          if Wire_Values (Name (1), Name (2)).Holds_Value then
             return Wire_Values (Name (1), Name (2)).Value;
          end if;
+         --  we don't know the value, so evaluate it recursively
          Name_Value :=
            (case Wire.Gate is
-              when Value => Evaluate (Wire.Value),
+              when Value => Evaluate (Wire.Input),
 
-              when Not_Gate => not Evaluate (Wire.Value),
+              when Not_Gate => not Evaluate (Wire.Input),
 
               when And_Gate =>
-                Evaluate (Wire.Values.Left) and Evaluate (Wire.Values.Right),
+                Evaluate (Wire.Inputs.Left) and Evaluate (Wire.Inputs.Right),
 
               when Or_Gate =>
-                Evaluate (Wire.Values.Left) or Evaluate (Wire.Values.Right),
+                Evaluate (Wire.Inputs.Left) or Evaluate (Wire.Inputs.Right),
 
               when Left_Shift =>
                 Interfaces.Shift_Left
-                  (Evaluate (Wire.Values.Left),
-                   Natural (Evaluate (Wire.Values.Right))),
+                  (Evaluate (Wire.Inputs.Left),
+                   Natural (Evaluate (Wire.Inputs.Right))),
 
               when Right_Shift =>
                 Interfaces.Shift_Right
-                  (Evaluate (Wire.Values.Left),
-                   Positive (Evaluate (Wire.Values.Right))));
+                  (Evaluate (Wire.Inputs.Left),
+                   Positive (Evaluate (Wire.Inputs.Right))));
+         --  record it in the memo before returning
          Wire_Values (Name (1), Name (2)) :=
            (Holds_Value => True, Value => Name_Value);
          return Name_Value;
@@ -266,12 +302,17 @@ procedure Day07 is
 
    function Part_2 (Value_Of_A : Value_Type) return Value_Type is
    begin
+      --  reset wires, then set b to a's value
       Wire_Values :=
         [others => [others => Maybe_Value'(Holds_Value => False)]];
       Wire_Values ('`', 'b') :=
         Maybe_Value'(Holds_Value => True, Value => Value_Of_A);
+
       return Evaluate (Input_Record'(Gate, "`a"));
    end Part_2;
+
+   --  SECTION
+   --  main
 
 begin
    Read_Input;
